9ada4fdbb79ad06ac8c3fb7e2005947ee6cfd255
Scott Idler <scott.a.idler@gmail.com>
2018-04-27 09:48:12 -0700

    added script to create hosts file and generate ansible commands

---
+++ vulns2ansible.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
+import re
+import sys
+sys.dont_write_bytecode = True
+
+from pprint import pprint
+
+class InventorySpecifiedError(Exception):
+    def __init__(self):
+        message = 'do not specify inventory; script will generate it'
+        super(InventorySpecifiedError, self).__init__(message)
+
+class ModuleArgsSpecifiedError(Exception):
+    def __init__(self):
+        message = 'do not specify module args; script will generate it'
+        super(ModuleArgsSpecifiedError, self).__init__(message)
+
+def get_pkgs2hosts(vulns):
+    pkgs2hosts  = {}
+    regex = re.compile('([^,]+),([^,]+),([^,]+)')
+    for vuln in vulns:
+        match = regex.search(vuln)
+        if match:
+            host, _, pkgs = match.groups()
+            pkgs2hosts[pkgs] = pkgs2hosts.get(pkgs, []) + [host]
+    return pkgs2hosts
+
+def gen_cmds(pkgs2hosts, args):
+    cmds = []
+    for pkgs, hosts in pkgs2hosts.items():
+        name = pkgs.replace(' ', '-') + '.hosts'
+        with open(name, 'w') as f:
+            f.write('\n'.join(hosts)+'\n')
+        cmds += ["ansible -i {name} -a 'sudo yum-wrapper update -y {pkgs}' {args}".format(**locals())]
+    return cmds
+
+def v2a(vulns, args):
+    pkgs2hosts = get_pkgs2hosts(vulns)
+    cmds = gen_cmds(pkgs2hosts, args)
+    print('\n'.join(cmds))
+
+if __name__ == '__main__':
+    vulns =sys.stdin.read().strip().split('\n')
+    args = sys.argv[1:]
+    if '-h' in args or '--help' in args:
+        os.system('ansible --help')
+        sys.exit(-1)
+    elif '-i' in args:
+        raise InventorySpecifiedError
+    elif '-a' in args:
+        raise ModuleArgsSpecifiedError
+    v2a(vulns, '\n'.join(args))
